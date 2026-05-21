0ff7db8822431638176c7902671f34303231b1cd
Westley Sturhan <westley@thinkmoco.com>
2026-05-21 12:56:33 -0400

    Supabase 2.0

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.FLYER_BACKEND_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ybhixwkuusmznwtjhtoa.supabase.co",
+  supabaseAnonKey: "sb_publishable_TteSNk44RMkREegSYFw3dg_8sdRjRUj",
   tableName: "flyer_entries",
   bucketName: "flyer-photos"
 };
